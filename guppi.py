fdd6086e29e2b5da136152aed568d6ac0ae15010
Khaled Allen <khaled.allen@gmail.com>
2021-01-22 15:06:56 -0700

    why are we not updated?

--- guppi.py
+++ guppi.py
@@ -22,12 +22,57 @@ def main():
     #audioInterface = Voice()
     #intent = audioInterface.get_intent()
 
+    def listen():
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+            print("Processing...")
+
+        try:
+            speech = r.recognize_wit(audio, key=WIT_AI_KEY, show_all=True)
+            print(speech)
+            intent = speech['entities']['intent'][0]['value']
+
+            if intent == "task_create":
+                try:
+                    todo = TaskManager(config['dbName'])
+                    task_name = speech['entities']['task_name'][0]['value'] 
+                    todo.add(task_name, quick_input=True)
+                    todo.list()
+                except:
+                    print("There was an error")
+            if intent == "task_complete":
+                try:
+                    todo = TaskManager(config['dbName'])
+                    task_id = speech['entities']['number'][0]['value']
+                    todo.complete(task_id)
+                except:
+                    print("There was an error")
+            if intent == "task_clear":
+                try:
+                    todo = TaskManager(config['dbName'])
+                    todo.clear(force=True)
+                except:
+                    print("There was an error")
+            if intent == "exit":
+                print("exiting")
+            return
+
+        except sr.UnknownValueError:
+            print("Wit.ai could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Wit.ai service; {0}".format(e))
+
+
     while True:
         # obtain audio from the microphone
         r = sr.Recognizer()
         with sr.Microphone() as source:
             print("Say something!")
             audio = r.listen(source)
+            print("Processing...")
 
         # recognize speech using Wit.ai
         WIT_AI_KEY = config['witApiKey']
@@ -35,21 +80,12 @@ def main():
         try:
             speech = r.recognize_wit(audio, key=WIT_AI_KEY, show_all=True)
             print(speech)
-            intent = speech['entities']['intent'][0]['value']
-            if intent == "task_create":
-                todo = TaskManager(config['dbName'])
-                task_name = speech['entities']['task_name'][0]['value'] 
-                todo.add(task_name, quick_input=True)
-                todo.list()
-            if intent == "task_complete":
-                todo = TaskManager(config['dbName'])
-                task_id = speech['entities']['number'][0]['value']
-                todo.complete(task_id)
-            if intent == "task_clear":
-                todo = TaskManager(config['dbName'])
-                todo.clear(force=True)
-            if intent == "exit":
-                break
+            if speech['entities'] and speech['entities']['intent']:
+                intent = speech['entities']['intent'][0]['value']
+                if intent == "listen":
+                    print("Listening")
+                    listen()
+
         except sr.UnknownValueError:
             print("Wit.ai could not understand audio")
         except sr.RequestError as e:
